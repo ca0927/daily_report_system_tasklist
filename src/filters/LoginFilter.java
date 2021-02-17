c692d36370675f0d977670c365797eb6acc7041b
c.a <cooglecocoa@gmail.com>
2021-02-17 21:36:22 +0900

    Add login / logout link

---
+++ src/filters/LoginFilter.java
@@ -0,0 +1,62 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import models.Employee;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter("/*")
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor.
+     */
+    public LoginFilter() {
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String context_path = ((HttpServletRequest)request).getContextPath();
+        String servlet_path = ((HttpServletRequest)request).getServletPath();
+
+        if(!servlet_path.matches("/css.*")) {      // CSS フォルダ内は認証処理から除外する
+            HttpSession session = ((HttpServletRequest)request).getSession();
+
+            // セッションスコープに保存された従業員（ログインユーザ）情報を取得
+            Employee e = (Employee)session.getAttribute("login_employee");
+
+//            if()
+//
+//        }
+
+
+//        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+}
